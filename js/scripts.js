70858ad6e4a8ef5660386cb3b7552804b71c8ca3
Naiviv <nnaiviv@eagles.ewu.edu>
2019-06-05 11:28:21 -0700

    Added Porfolio
    *Chocolat added
    * Updated Nav

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,16 @@ console.log('OK');
 
 // Page Headings
 $('h1').lettering();
+
+
+
+
+
+//Portfolio
+
+$('.portfolio').Chocolat({
+
+    separator2: '--',
+    fullScreen: true,
+    enableZoom: false,
+});
